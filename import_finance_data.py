737ef61e547ca001ec2bcc03e212f33a491c6342
Aaron Clark <clarka@DataCenter.hambeach.com>
2022-05-16 23:30:14 -0400

    second

--- import_finance_data.py
+++ import_finance_data.py
@@ -3,19 +3,25 @@
 '''
 File to follow along with the Udemy Colurse Importing Free Financial Data with Python
 
-    + course: https://www.udemy.com/course/importing-free-financial-data-with-python/
-    + teacher: Alexander Hagmann
-    + resources: 
+    + Course: https://www.udemy.com/course/importing-free-financial-data-with-python/
+    + Teacher: Alexander Hagmann
+    + Resources: 
         - https://pypi.org/project/yfinance/
-        - 
-
     
     + Definitions:
         - Dividends (payout)
         - Stock Splits (why?)
         - Indexes (Dow Jones, S&P 500)
             -Total Return Index?
+        - Mutual Funds
+        - ETF (exchange-traded fund)
 
+    + Forms:
+        -Balance Sheet
+        -P&L
+        -Cashflow
+
+    
 '''
 
 
@@ -38,11 +44,12 @@ import pandas as pd
 import time
 import inspect
 from datetime import datetime, date, timedelta# import time
-
 import matplotlib.pyplot as plt
 
 import yfinance as yf
 
+
+pd.options.display.max_rows = 999
 
 # APPEND REPO PATH
 #======================
@@ -152,6 +159,7 @@ indexes = yf.download(ticker, period = '5y').Close
 #%% ============================================================================================
 
 # Importing Currencies / FX
+#========================
 
 ticker = 'EURUSD=X'
 ticker2 = 'USDEUR=X'
@@ -165,6 +173,7 @@ gbp_usd = yf.download(ticker3, period = '5y')
 #%% ============================================================================================
 
 # Importing Crypto Currencies
+#========================
 
 ticker = ['BTC-USD', 'ETH-USD']
 
@@ -175,10 +184,94 @@ plt.show()
 
 
 
+#%% ============================================================================================
+
+# Importing Funds & ETFs
+#========================
+
+ticker = 'TLT' #iShares 20+ Year Treasury Bond ETF
+ticker2 = 'OMOIX' #Vivaldi Multi-Strategy Fund Class
+
+yf.download(ticker, period = '5y')
+yf.download(ticker2, period = '5y')
 
 
 
 
+#%% ============================================================================================
+
+# Importing Treasury Yields
+#========================
+
+ticker1 = '^TNX' # 10Y Treasury Yield
+ticker2 = '^FVX' # 5Y Treasury Yield
+
+ticker = ['^TNX','^FVX']
 
 
+yeilds = yf.download(ticker, period = '5y').Close
+yf.download(ticker2, period = '5y')
+
+yeilds.plot()
+plt.show()
+
+
+#%% ============================================================================================
+
+# Creating Ticker Object
+#========================
+
+ticker= 'DIS' #disney
+
+dis = yf.Ticker(ticker)
+
+dis.ticker
+dis.history(period = '1d', inverval = '1m') #this data is ALREADY ADJUSTED!!!
+# -- Alex typically doesn't use the history methods
+
+
+#%% ============================================================================================
+
+# Stock Fundamentals, Meta Data, Performance Metrics
+#========================
+
+dis.info
+
+d = pd.Series(dis.info)
+df = pd.Series(dis.info, name = 'DIS').to_frame().T
+
+
+# add additional rows to df
+ticker = ['MSFT', 'FB']
+for i in ticker:
+    df.loc[i] = pd.Series(yf.Ticker(i).info)
+
+
+
+#%% ============================================================================================
+
+# Importing Financials (Balance Sheet, P&L, Cashflows)
+#========================
+
+# ticker= 'DIS' #disney
+
+# dis = yf.Ticker(ticker)
+
+# dis.balance_sheet
+# dis.financials
+# dis.cashflow
+
+ticker= 'HBB'
+
+hbb = yf.Ticker(ticker)
+
+hbb.balance_sheet
+hbb.financials #P&L Statement
+hbb.cashflow
+
+ticker= ['HBB', 'GE']
+
+
+for i in ticker:
+    yf.Ticker(i).financials.to_csv(f'{i}.csv')
 
